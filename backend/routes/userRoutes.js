c253d8ee5323e0648b85e7e42e74b28503c15b5e
kosman2015 <105610722+kosman2015@users.noreply.github.com>
2024-01-16 15:49:01 -0500

    added middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,7 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import {authUser} from '../controllers/userController.js';
+import { authUser } from "../controllers/userController.js";
 
-
-router.post('/auth', authUser);
+router.post("/auth", authUser);
 
 export default router;
