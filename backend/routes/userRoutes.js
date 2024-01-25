d0f6a07ab2a44dec317cf3373c13b79557b8ca4d
kosman2015 <105610722+kosman2015@users.noreply.github.com>
2024-01-25 17:41:20 -0500

    added route for creating post

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,12 +6,14 @@ import {
   logoutUser,
   getUserProfile,
   updateUserProfile,
+  createNewPost,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
+router.post("/new", protect, createNewPost);
 router
   .route("/profile")
   .get(protect, getUserProfile)
