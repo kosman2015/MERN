a2fb1d41522461d723b7e587663c069e8fbce999
kosman2015 <105610722+kosman2015@users.noreply.github.com>
2024-01-27 17:21:11 -0500

    added route for created posts

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { createNewPost } from "../controllers/postsController.js";
+import { createNewPost, getPost } from "../controllers/postsController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/new", protect, createNewPost);
-
+router.get("/:_id", getPost);
 export default router;
