0d5bb8a82337715dd46181a6fe831956dbca7524
kosman2015 <105610722+kosman2015@users.noreply.github.com>
2024-05-23 17:41:56 -0400

    Update PostsScreen.jsx

---
+++ frontend/src/screens/PostsScreen.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useGetPostsMutation } from "../slices/postsApiSlice";
+
+function PostsScreen() {
+  return <div>PostsScreen</div>;
+}
+
+export default PostsScreen;
