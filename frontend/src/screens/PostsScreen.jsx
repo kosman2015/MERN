14c4c946541ef11d00eadd5b7e431f40c9efded8
kosman2015 <105610722+kosman2015@users.noreply.github.com>
2024-05-29 15:19:35 -0400

    Show all posts on frontend

--- frontend/src/screens/PostsScreen.jsx
+++ frontend/src/screens/PostsScreen.jsx
@@ -1,17 +1,37 @@
+import Loader from "../components/Loader";
 import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { useGetPostsQuery } from "../slices/postsApiSlice";
 
 function PostsScreen() {
-  const { data } = useGetPostsQuery();
-  console.log(data);
-  if (data) {
-    data.forEach((element) => {
-      console.log(element._id);
-    });
-  }
-  return <div>PostsScreen</div>;
+  const [posts, setPosts] = useState(null);
+  const { data, isLoading, isError } = useGetPostsQuery();
+
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      setPosts(data);
+    }
+  }, [data, isLoading, isError]);
+
+  return (
+    <div>
+      <h1>PostsScreen</h1>
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <div>Error: {isError.message}</div>
+      ) : (
+        posts &&
+        posts.map((post) => (
+          <div key={post._id}>
+            <h2>{post.title}</h2>
+            <h3>By: {post.author.name}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default PostsScreen;
